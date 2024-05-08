10fdd989edc2408ee351f5aa73c3e0005f320998
Satyasree05208591 <satya05208591@gmail.com>
2024-05-07 19:51:35 -0500

    pushed

--- devopsexample/src/main/java/org/devopsexample/DevopsBuddyApplication.java
+++ devopsexample/src/main/java/org/devopsexample/DevopsBuddyApplication.java
@@ -1,6 +1,6 @@
 package org.devopsexample;
 
-public class Main {
+public class DevopsBuddyApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
